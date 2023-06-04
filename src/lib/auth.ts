18dc423aa603bba8ca2996a5ee3ec3bb7e29f77e
patrykharasiuk <patryk.harasiuk@op.pl>
2023-06-04 15:50:23 +0200

    feat: extend Session module from nextaut

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -51,7 +51,7 @@ export const authOptions: NextAuthOptions = {
     },
     jwt: ({ token, user }) => {
       if (user) {
-        const u = user as unknown as any;
+        const u = user;
         return {
           ...token,
           id: u.id,
