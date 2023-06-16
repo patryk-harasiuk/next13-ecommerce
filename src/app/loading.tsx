9832b7a593ec89dee568abd9dfd7ce9da8a897cc
patrykharasiuk <patryk.harasiuk@op.pl>
2023-06-16 09:11:07 +0200

    feat: add button variants

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
 
 export function Loading() {
-  return <LoadingSpinner />;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <LoadingSpinner />
+    </div>
+  );
 }
 
 export default Loading;
--- src/app/profile/error.tsx
+++ src/app/profile/error.tsx
@@ -12,7 +12,7 @@ export default function Error({ error, reset }: Props) {
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex flex-col gap-y-4">
         <h2 className="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">
-          Something went wrong!
+          Something went wrong while fetching user data
         </h2>
         <PrimaryButton onClick={reset}>Try Again</PrimaryButton>
       </div>
