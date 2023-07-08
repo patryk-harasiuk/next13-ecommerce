16d4fc860b1ac047de8ceb478bd09d6d039c6235
patrykharasiuk <patryk.harasiuk@op.pl>
2023-07-08 11:48:48 +0200

    feat: add Shell.tsx component

---
+++ src/components/shell.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/utils/cn';
+
+interface Props
+  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  children: React.ReactNode;
+  layout?: 'default' | 'dashboard' | 'auth';
+}
+export function Shell({ children, layout, className, ...props }: Props) {
+  return (
+    <section
+      className={cn(
+        'flex items-center gap-8 pb-8 pt-6 md:py-8',
+        layout === 'default' && 'container',
+        layout === 'auth' && 'container max-w-lg',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </section>
+  );
+}
+
+export default Shell;
