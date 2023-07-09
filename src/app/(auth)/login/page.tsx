4c25a5e8a20b38f0098f0c2286c83f5fd03ce081
patrykharasiuk <patryk.harasiuk@op.pl>
2023-07-09 14:06:40 +0200

    feat: add styles for login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,26 @@
-import Image from 'next/image';
+import { Metadata } from 'next';
 
 import LoginForm from '@/components/forms/login-form';
+import Shell from '@/components/shell';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
-const CompanyLogo = '/assets/images/company-logo.svg';
+export const metadata: Metadata = {
+  title: 'Log in to your account',
+};
 
 const Login = (): JSX.Element => (
-  <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-      <Image width={30} height={10} className="mx-auto" src={CompanyLogo} alt="Company logo" />
-      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-        Sign in to your account
-      </h2>
+  <Shell layout="auth">
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Sign in</CardTitle>
+        <CardDescription>Sign in to your account</CardDescription>
+      </CardHeader>
 
-      <LoginForm />
-    </div>
-  </div>
+      <CardContent>
+        <LoginForm />
+      </CardContent>
+    </Card>
+  </Shell>
 );
 
 export default Login;
--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -4,15 +4,15 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import * as z from 'zod';
 
 import TextInput from '@/components/ui/text-input';
-import { cn } from '@/utils/cn';
 
-import { buttonVariants } from '../ui/button';
+import { Icons } from '../icon';
+import { Button } from '../ui/button';
 import ErrorBox from '../ui/error-box';
 
 type LoginInputs = {
@@ -29,6 +29,7 @@ const LoginForm = (): JSX.Element => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [error, setError] = useState<string>('');
+  const [isPending, startTransition] = useTransition();
 
   const {
     handleSubmit,
@@ -39,31 +40,29 @@ const LoginForm = (): JSX.Element => {
   });
 
   const onSubmit = async (values: LoginInputs) => {
-    try {
-      setError('');
+    setError('');
 
-      const signInResult = await signIn('credentials', {
-        redirect: false,
-        callbackUrl: searchParams?.get('from') || '/',
-        ...values,
-      });
+    startTransition(async () => {
+      try {
+        const signInResult = await signIn('credentials', {
+          redirect: false,
+          callbackUrl: searchParams?.get('from') || '/',
+          ...values,
+        });
 
-      if (signInResult?.error === 'CredentialsSignin')
-        return setError('Email or password is wrong');
+        if (signInResult?.error === 'CredentialsSignin')
+          return setError('Email or password is wrong');
 
-      router.push('/');
-    } catch {
-      toast.error('Unexpected error, please try again', {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
+        router.push('/');
+      } catch {
+        toast.error('Unexpected error, please try again', {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+    });
   };
   return (
-    <form
-      method="POST"
-      onSubmit={handleSubmit(onSubmit)}
-      className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md"
-    >
+    <form method="POST" onSubmit={handleSubmit(onSubmit)} className="mb-4 pb-8 pt-6">
       <TextInput type="text" registration={register('email')} label="Email" error={errors.email} />
 
       <TextInput
@@ -73,15 +72,16 @@ const LoginForm = (): JSX.Element => {
         error={errors.password}
       />
 
-      <button className={cn(buttonVariants())} type="submit">
-        Sign in
-      </button>
+      <Button type="submit" disabled={isPending} className="mt-4 w-full">
+        {isPending && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" aria-hidden="true" />}
+        Sign up
+      </Button>
 
       {error && <ErrorBox>{error}</ErrorBox>}
 
-      <p className="mt-6 text-center text-sm text-gray-600">
+      <p className="mt-6 text-center text-sm text-muted-foreground">
         Don&apos;t have an account yet?
-        <Link className="ml-1 font-bold text-indigo-600" href="/register">
+        <Link className="ml-1 font-bold text-primary" href="/register">
           Sign up
         </Link>
       </p>
