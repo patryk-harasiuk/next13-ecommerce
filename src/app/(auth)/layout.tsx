92141d2d1ad1cfa4775176ed352e8ae47a13893b
Patryk Harasiuk <patryk.harasiuk@op.pl>
2023-08-23 19:21:21 +0200

    feat: add AspectRatio component, add object-cover to image

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import { ReactNode } from 'react';
 
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+
 import AuthLayoutImage from '../../../public/assets/images/auth-screen.jpg';
 
 type Props = {
@@ -9,18 +11,19 @@ type Props = {
 
 export default function AuthLayout({ children }: Props) {
   return (
-    <div className="grid min-h-full grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
-      <div className="aspect-h-9 aspect-w-16 relative w-full">
+    <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
+      <AspectRatio ratio={16 / 9}>
         <Image
           src={AuthLayoutImage}
           alt="Hanging clothes"
           priority
           fill
-          className="absolute inset-0"
+          className="absolute inset-0 object-cover"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
         <div className="absolute inset-0" />
-      </div>
-      <main className="container absolute top-1/2 col-span-1 flex -translate-y-1/2  items-center justify-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
+      </AspectRatio>
+      <main className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
         {children}
       </main>
     </div>
